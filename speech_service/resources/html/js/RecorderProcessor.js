147a480612c67a71c393c7ce55c6b3e4517ee807
andrePankraz <andre@pankraz.de>
2022-10-18 23:00:41 +0200

    Further Audio Tests

--- speech_service/resources/html/js/RecorderProcessor.js
+++ speech_service/resources/html/js/RecorderProcessor.js
@@ -2,9 +2,9 @@ class RecorderProcessor extends AudioWorkletProcessor {
 
   constructor() {
     super()
-    this.bufferSize = 16000 * 5 // 16 kHz with 2 seconds
-    this._bytesWritten = 0
+    this.bufferSize = 16000 * 3 // 16 kHz with 3 seconds
     this._buffer = new Float32Array(this.bufferSize)
+    this._bytesWritten = 0
     this.initBuffer()
   }
 
@@ -25,15 +25,15 @@ class RecorderProcessor extends AudioWorkletProcessor {
     const samples = input[0].length
     let sample = 0
     for (let i = 0; i < samples; i++) {
+      if (i % 3 !== 0) {
+        // simple conversion from 48 kHz to 16 kHz by skipping 2 samples,
+        // reduce sampling rate here in worker to reduce data transmissions
+        continue
+      }
       for (let j = 0; j < channels; ++j) {
         sample += input[j][i]
       }
-      if (i % 3 !== 0) {
-        // simple 48000 -> 16000 kHz bei skipping, later interpolation?
-        // reduce here in worker to reduce data transmissions
-        continue
-      }
-      this._buffer[this._bytesWritten++] = sample / (samples * 3)
+      this._buffer[this._bytesWritten++] = sample / samples
       sample = 0
       if (this.isBufferFull()) {
         this.flush()
@@ -53,6 +53,7 @@ class RecorderProcessor extends AudioWorkletProcessor {
 
   process(inputs, outputs) {
     // input[n'th input][m'th channel] -> Float32Array with 128 sample values in range [-1 .. 1]
+    // 128 samples at 48 kHz is is 2.66 ms
     this.append(inputs[0])
     return true // keep processor alive
   }
